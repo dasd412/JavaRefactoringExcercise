2f879f022f8284f5155d61a2f3115e2e41d80651
dasd412 <45190017+dasd412@users.noreply.github.com>
2020-01-01 14:59:40 +0900

    .gitignore is changed

---
+++ RefactoringExcersie/magicNumber/commitTest/AfterGitIgnore.java
@@ -0,0 +1,11 @@
+package commitTest;
+
+public class AfterGitIgnore {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("After changing gitIngnore");
+	}
+
+}
